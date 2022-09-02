b8ad37898135e2264fe4e22756aa9cd1c1ebc9ef
sanchez-moon <sanchez@stayge.io>
2022-09-02 15:01:40 +0900

    fix: update package

--- dist/index.js
+++ dist/index.js
@@ -13842,6 +13842,17 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
@@ -13959,7 +13970,8 @@ var run = function () { return __awaiter(void 0, void 0, void 0, function () {
                 userNames = _a.sent();
                 userNames = userNames.filter(function (name) { return name !== github.context.actor; });
                 if (userNames.length == 0) {
-                    console.log("No users to be alerted");
+                    console.log("No Suggested Reviewer");
+                    octokit.issues.createComment(__assign(__assign({}, github.context.repo), { issue_number: request.number, body: "No Suggested Reviewer" }));
                     return [2 /*return*/];
                 }
                 message = "Your code will change with this PR!";
